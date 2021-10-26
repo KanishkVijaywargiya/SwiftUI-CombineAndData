f23ba17b4a723143f3e078732dd72b8662325ae1
Kanishk Vijaywargiya <secretcodersociety@gmail.com>
2021-10-26 12:21:59 +0530

    Initial Commit

---
+++ SwiftUICombineAndData/SwiftUICombineAndDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICombineAndDataApp.swift
+//  SwiftUICombineAndData
+//
+//  Created by KANISHK VIJAYWARGIYA on 26/10/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICombineAndDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
